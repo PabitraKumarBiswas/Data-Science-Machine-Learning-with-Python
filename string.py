490defef0d87f0f61b41601186e7a91893b85500
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-09-11 01:16:46 +0600

    creating string.py

---
+++ string.py
@@ -0,0 +1,4 @@
+# A string is a set of characters. The character can be numeric, alphabet or any thing.
+# A string can be written between single quotation. Example: 'This is a string'
+# A string can also be written between double quotation. Example: "This is also a string."
+
