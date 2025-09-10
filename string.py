d83dfbcb70022c7b7cd277e4e09ea4c7a844a860
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-09-11 01:22:57 +0600

    this is a string

--- string.py
+++ string.py
@@ -2,3 +2,12 @@
 # A string can be written between single quotation. Example: 'This is a string'
 # A string can also be written between double quotation. Example: "This is also a string."
 
+# Python variables are Dynamically Typed
+
+a = 'This is a String.'
+
+print(a)
+
+1 == "This is also a String."
+
+print(1) 
