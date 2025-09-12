15b980774605bd6c5579afc0fb4f27af44a55c96
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-09-12 23:59:07 +0600

    start

--- string.py
+++ string.py
@@ -1,6 +1,6 @@
 # A string is a set of characters. The character can be numeric, alphabet or any thing.
 # A string can be written between single quotation. Example: 'This is a string'
-# A string can also be written between double quotation. Example: "This is also a string."
+# A string can also be written between double or triple quotation. Example: "This is also a string."
 
 # Python variables are Dynamically Typed
 
@@ -10,9 +10,13 @@ print(a)
 
 1 == "This is also a String."
 
+b = """This is also a string"""
+print(b)
+
 # print(1) 
 
 print(type(a)) #it is str
 print(type(1)) #it is int
+print(type(b)) #it is str
 
-# class is like a framework.
+# class is like a framework or design.
