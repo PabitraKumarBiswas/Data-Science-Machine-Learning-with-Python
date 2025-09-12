b5c6ceaeb824a39ec4dc120ceeedd8c7be252c30
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-09-13 01:20:36 +0600

    len method

---
+++ str_index.py
@@ -0,0 +1,14 @@
+a = 'My name is Pabitra Kumar biswas.' 
+
+
+print(a[0])
+
+print(a[20])
+
+print(a[30])
+
+print(a[31])
+
+# print(a[33])
+
+print(len(a)) #len method describes lenght of a string.
