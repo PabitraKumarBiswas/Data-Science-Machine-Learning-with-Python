5203b2d48252c21076e6682e052bfb23ca8a1f60
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-09-17 02:38:06 +0600

    operater precedence

--- operator.py
+++ operator.py
@@ -2,5 +2,15 @@ a = 10 + 5
 b = 10 - 5
 c = 10 * 5
 d = 10 / 5 #float division
+e = 43//5 # // means integer division. it gives the integer output only
+f = 43/5
+g = 5 ** 2 # 5 to the power 2
 
-print(a,"\n",b,"\n",c,"\n",d)
+# print(a,"\n",b,"\n",c,"\n",d)
+
+print(a, b, c, d, e)
+print(a, b, c, d, float(e), int(f), g) # int(value) also means the integer division
+
+x = (2+4) - 5 + 18/2 + 12*2 - 5**2 # ** means exponential that means power of a number or value
+
+print(x)
