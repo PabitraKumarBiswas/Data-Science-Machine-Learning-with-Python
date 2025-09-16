16b433cfa82d1b066625577153bef5b9229b7b9b
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-09-17 01:52:39 +0600

    string_format update 1

--- string_format.py
+++ string_format.py
@@ -1,6 +1,5 @@
-a= "Good night, Priya. Have a good bad night!"
+# a= "Good night, Priya. Have a good bad night!"
 
 user_input = input("what is your name?")
-
-print(user_input)
+a = "Good night, {}. Have a Good Bad nigh t.{} Have you done your homework?".format(user_input, user_input)
 print(a)
