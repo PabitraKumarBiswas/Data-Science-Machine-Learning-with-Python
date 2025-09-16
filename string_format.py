a2eac4f2936875886b16ba181ea2a18a3901d84e
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-09-17 02:05:00 +0600

    update string_formate.py

--- string_format.py
+++ string_format.py
@@ -1,5 +1,18 @@
-# a= "Good night, Priya. Have a good bad night!"
+# # a= "Good night, Priya. Have a good bad night!"
 
-user_input = input("what is your name?")
-a = "Good night, {}. Have a Good Bad nigh t.{} Have you done your homework?".format(user_input, user_input)
-print(a)
+# user_input = input("what is your name?")
+# a = "Good night, {}. Have a Good Bad nigh t.{} Have you done your homework?".format(user_input, user)
+# print(a)
+
+
+age = 25
+fname= "Pabitra"
+mname= "Kumar"
+lname= "Biswas"
+#long Process
+txt= "Hi, i am {fname} {mname} {lname}. I am {age} years old.".format(mname=mname, fname=fname, lname=lname, age=age)
+
+#short technique
+txt2= f"Hello, {fname} {mname} {lname}. i am {age}. have a Good bad night"
+print(txt)
+print(txt2)
