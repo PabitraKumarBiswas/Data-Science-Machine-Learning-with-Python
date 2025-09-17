a629f2bd29ac7dea88a292dd8d14b3b9401ccbd5
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-09-18 01:23:31 +0600

    mid at list_touple

---
+++ conditionals.py
@@ -0,0 +1,27 @@
+#conditional statement
+
+#if
+#else
+
+# print(10==4)
+# print(10==10)
+
+rain = 1 # bristy hocche
+
+if rain == 1: 
+    print("aj ghumabo")
+else:
+    print("tao ghumabo")
+
+
+# ekta number positive negative or zero kina check korbo.
+
+# a = 10
+a = 0
+
+if a > 0:
+    print("A is positve.")
+elif a < 0:
+    print("A is negative")
+else:
+    print("A is zero")
---
+++ list_tuples.py
@@ -0,0 +1,9 @@
+a = [1 , 2, 3, 'Mona', 'Pulak', 8.9, 3.4]
+
+#list is mutable. that means we can update the list later
+
+a[3] = 'Pabitra'
+
+print(a)
+print(a[-1])
+print(len(a))
