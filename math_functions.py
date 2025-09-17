a6aab84e17bdfd9ab79f3a3d598adbd140d1a245
PabitraKumarBiswas <pabitra.pust@gmail.com>
2025-09-18 00:51:57 +0600

    end math functions

---
+++ math_functions.py
@@ -0,0 +1,20 @@
+#ceil floor and round all works for float value.  
+import math
+
+#ceil
+x = 4.5 # 5
+y = 2.1
+print(math.ceil(x))
+print(math.ceil(y))
+
+#floor
+a = 4.5 #4
+b = 0.00004
+print(math.floor(a))
+print(math.floor(b))
+
+#round
+i = 4.49
+j = 4.50 
+print(round(i))
+print(round(j))
